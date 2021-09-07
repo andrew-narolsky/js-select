d0f966b7c326df52c089779e8d0b222ec582910b
Andrew <andrew.narolsky@gmail.com>
2021-09-07 13:58:24 +0300

    init select

--- README.md
+++ README.md
@@ -1,17 +1,9 @@
-# Gulp + Sass
+# JS select
 
-## Installation (development)
-
-1. Clone repository
+Init select
 ```
-git clone https://github.com/andrew-narolsky/essaypenguins.git
-```
-
-2. Run npm install
-```
-npm install
-```
-3. Start project
-```
-npm start
+let formStyler = new FormStyler({
+    selector: 'select',
+});
+formStyler.init();
 ```
--- js/script.js
+++ js/script.js
@@ -1,1 +1,59 @@
+const FormStyler = function(options) {
 
+    this.selector = document.querySelector(options.selector);
+
+    this.init = function () {
+        this.selector.style.display = 'none';
+        let lists = '';
+        let title = '';
+        let options = this.selector.querySelectorAll('option');
+        for (let option in options) {
+            if (options.hasOwnProperty(option)) {
+                if (option === '0') {
+                    title = options[option].innerText;
+                }
+                lists += '<li data-id="' + options[option].value + '">' + options[option].innerText + '</li>';
+            }
+        }
+        this.selector.insertAdjacentHTML('afterend', '<div class="swf-list">' +
+            '<div class="title">' + title + '</div>' +
+            '<div class="list-wrapper">' +
+            '<ul>' + lists + '</ul>' +
+            '</div>' +
+            '</div>');
+        this.clickOnTitle();
+        this.selectOption();
+        this.closeOnBody();
+    }
+
+    this.clickOnTitle = function () {
+        let title = this.selector.nextSibling.querySelector('.title');
+        title.addEventListener('click',  (event) => {
+            event.stopPropagation();
+            this.selector.nextSibling.classList.toggle('opened');
+        });
+    }
+
+    this.selectOption = function () {
+        let title = this.selector.nextSibling.querySelector('.title');
+        let lists = this.selector.nextSibling.querySelectorAll('.list-wrapper ul li');
+        let select = this.selector;
+        for (let li in lists) {
+            if (lists.hasOwnProperty(li)) {
+                lists[li].addEventListener('click',  function () {
+                    let id = lists[li].getAttribute('data-id');
+                    title.innerText = lists[li].innerText;
+                    select.value = id;
+                    select.nextSibling.classList.remove('opened');
+                });
+            }
+        }
+    }
+
+    this.closeOnBody = function () {
+        let body = document.querySelector('body');
+        body.addEventListener('click',  () => {
+            this.selector.nextSibling.classList.remove('opened');
+        });
+    }
+}
