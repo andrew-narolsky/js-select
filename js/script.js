82670e02b9b6b73cd53353906f4b834153f36260
Andrew <andrew.narolsky@gmail.com>
2021-09-21 14:41:01 +0300

    add value method

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
-const FormStyler = function(options) {
+const FormStyler = function (options) {
     this.selectors = document.querySelectorAll(options.selectors);
     this.countItems = options.countItems;
     this.init();
+
+    this.value = function (selector) {
+        return document.querySelector(selector).value;
+    }
 }
 
-FormStyler.prototype.init = function() {
+FormStyler.prototype.init = function () {
     for (let $selector in this.selectors) {
         if (this.selectors.hasOwnProperty($selector)) {
             this.selectors[$selector].style.display = 'none';
@@ -32,7 +36,7 @@ FormStyler.prototype.init = function() {
     this.closeOnBody();
 }
 
-FormStyler.prototype.changeHeight = function(countItems, $element) {
+FormStyler.prototype.changeHeight = function (countItems, $element) {
     let $listItem = $element.nextSibling.querySelector('.list-wrapper ul li');
     let listItemsCounts = $element.nextSibling.querySelectorAll('.list-wrapper ul li').length;
     if (listItemsCounts > countItems) {
@@ -43,12 +47,12 @@ FormStyler.prototype.changeHeight = function(countItems, $element) {
     $element.nextSibling.style.top = $listItem.offsetHeight + 'px';
 }
 
-FormStyler.prototype.clickOnTitle = function() {
+FormStyler.prototype.clickOnTitle = function () {
     let countItems = this.countItems;
     for (let $selector in this.selectors) {
         if (this.selectors.hasOwnProperty($selector)) {
             let $title = this.selectors[$selector].nextSibling.querySelector('.title');
-            $title.addEventListener('click',  function(event) {
+            $title.addEventListener('click',  function (event) {
                 event.stopPropagation();
                 event.target.closest('.swf-list').classList.toggle('opened');
                 FormStyler.prototype.changeHeight(countItems, event.target);
@@ -57,7 +61,7 @@ FormStyler.prototype.clickOnTitle = function() {
     }
 }
 
-FormStyler.prototype.selectOption = function() {
+FormStyler.prototype.selectOption = function () {
     for (let $selector in this.selectors) {
         if (this.selectors.hasOwnProperty($selector)) {
             let $title = this.selectors[$selector].nextSibling.querySelector('.title');
@@ -65,7 +69,7 @@ FormStyler.prototype.selectOption = function() {
             let $select = this.selectors[$selector];
             for (let $li in $lists) {
                 if ($lists.hasOwnProperty($li)) {
-                    $lists[$li].addEventListener('click',  function(event) {
+                    $lists[$li].addEventListener('click',  function (event) {
                         event.stopPropagation();
                         let id = $lists[$li].getAttribute('data-id');
                         $title.innerText = $lists[$li].innerText;
@@ -78,11 +82,11 @@ FormStyler.prototype.selectOption = function() {
     }
 }
 
-FormStyler.prototype.closeOnBody = function() {
+FormStyler.prototype.closeOnBody = function () {
     let $body = document.querySelector('body');
     for (let $selector in this.selectors) {
         if (this.selectors.hasOwnProperty($selector)) {
-            $body.addEventListener('click',  function(event) {
+            $body.addEventListener('click',  function (event) {
                 let $selects = event.target.querySelectorAll('.swf-list');
                 for (let i in $selects) {
                     if ($selects.hasOwnProperty(i)) {
